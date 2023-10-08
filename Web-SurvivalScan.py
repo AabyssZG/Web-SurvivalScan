281cec4ff62b0b47797e6c7c1b93bcdf2fc4e4e8
曾哥 <54609266+AabyssZG@users.noreply.github.com>
2023-10-08 13:31:35 +0800

    Update Web-SurvivalScan.py

--- Web-SurvivalScan.py
+++ Web-SurvivalScan.py
@@ -181,7 +181,7 @@ def main():
         cprint(f"[.] 正在检测目标URL " + url,"cyan")
         try:
             _thread.start_new_thread(lambda url: scanLogger(survive(url,proxies)), (url, ))
-            time.sleep(2)
+            time.sleep(0.6)
         except KeyboardInterrupt:
             print("Ctrl + C 手动终止了进程")
             sys.exit()
